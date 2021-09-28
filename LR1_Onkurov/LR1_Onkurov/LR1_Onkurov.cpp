f54e7097468a6d9bca1c4efd3ff6e9eb13ec2746
minya <minia.onkurov@mail.ru>
2021-09-28 15:19:48 +0300

    Попытка сделать меню

--- LR1_Onkurov/LR1_Onkurov/LR1_Onkurov.cpp
+++ LR1_Onkurov/LR1_Onkurov/LR1_Onkurov.cpp
@@ -1,20 +1,43 @@
-// LR1_Onkurov.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 int main()
 {
-    cout << "Hello World!\n";
+    
+}
+void Menu()
+{
+    system("cls");
+    cout << "1. Добавить трубу" << endl;
+    cout << "2. Добавить КС" << endl;
+    cout << "3. Просмотр всех объектов" << endl;
+    cout << "4. Редактировать трубу" << endl;
+    cout << "5. Редактировать КС" << endl;
+    cout << "6. Сохранить" << endl;
+    cout << "7. Загрузить" << endl;
+    cout << "0. Выход" << endl;
+    cout << ">";
+}
+int Get_NumberOfMenu (int inputNumber)
+{
+    int number;
+    if (inputNumber<8 && inputNumber>=0)
+    {
+        return inputNumber;
+    }
+    else
+    {
+        while (true)
+        {
+            cout << "Неверный ввод" << endl;
+            cin >> number;
+
+        }
+    }
+   
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
